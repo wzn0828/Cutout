97d6e699cc415710de248c676c8f3ed1b8c6d1ef
wzn0828 <18001279185@163.com>
2020-08-14 13:14:13 +0800

    Add MMA loss, AutoAugment

--- train.py
+++ train.py
@@ -2,13 +2,16 @@
 # run train.py --dataset cifar100 --model resnet18 --data_augmentation --cutout --length 8
 # run train.py --dataset svhn --model wideresnet --learning_rate 0.01 --epochs 160 --cutout --length 20
 
+import os
 import pdb
 import argparse
 import numpy as np
 from tqdm import tqdm
+import random
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.autograd import Variable
 import torch.backends.cudnn as cudnn
 from torch.optim.lr_scheduler import MultiStepLR
@@ -18,6 +21,7 @@ from torchvision import datasets, transforms
 
 from util.misc import CSVLogger
 from util.cutout import Cutout
+from util.autoaugment import SVHNPolicy
 
 from model.resnet import ResNet18
 from model.wide_resnet import WideResNet
@@ -46,18 +50,46 @@ parser.add_argument('--length', type=int, default=16,
                     help='length of the holes')
 parser.add_argument('--no-cuda', action='store_true', default=False,
                     help='enables CUDA training')
-parser.add_argument('--seed', type=int, default=0,
+parser.add_argument('--seed', type=int, default=123,
                     help='random seed (default: 1)')
 
 args = parser.parse_args()
-args.cuda = not args.no_cuda and torch.cuda.is_available()
-cudnn.benchmark = True  # Should make training should go faster for large models
 
+## ---local config ---##
+os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+
+args.dataset = 'svhn'
+args.use_extra = False
+args.model = 'wideresnet'
+args.checkpoint = 'Experiments/SVHN/svhn-noextra_wrn-16-8_cutout_autoaugment_mma1.0'
+
+args.mma = True
+args.mma_weight = 1.0
+
+args.auto_augment = True
+
+args.cutout = True
+args.length = 20
+
+args.learning_rate = 0.01
+args.epochs = 160
+
+args.data_path = '/home/wzn/Datasets/Classification/SVHN/'
+## ---local config ---##
+
+if not os.path.isdir(args.checkpoint):
+    os.makedirs(args.checkpoint)
+
+args.cuda = not args.no_cuda and torch.cuda.is_available()
+# Random seed, for deterministic behavior
+cudnn.benchmark = False
+cudnn.deterministic = True
+random.seed(args.seed)
 torch.manual_seed(args.seed)
 if args.cuda:
-    torch.cuda.manual_seed(args.seed)
+    torch.cuda.manual_seed_all(args.seed)
+# still need to set the work_init_fn to random.seed in train_dataloader, if multi numworkers
 
-test_id = args.dataset + '_' + args.model
 
 print(args)
 
@@ -69,14 +101,22 @@ else:
     normalize = transforms.Normalize(mean=[x / 255.0 for x in [125.3, 123.0, 113.9]],
                                      std=[x / 255.0 for x in [63.0, 62.1, 66.7]])
 
-train_transform = transforms.Compose([])
-if args.data_augmentation:
-    train_transform.transforms.append(transforms.RandomCrop(32, padding=4))
-    train_transform.transforms.append(transforms.RandomHorizontalFlip())
-train_transform.transforms.append(transforms.ToTensor())
-train_transform.transforms.append(normalize)
-if args.cutout:
-    train_transform.transforms.append(Cutout(n_holes=args.n_holes, length=args.length))
+
+if args.auto_augment:
+    train_transform = transforms.Compose(
+        [SVHNPolicy(),
+         transforms.ToTensor(),
+         Cutout(n_holes=args.n_holes, length=args.length),  # (https://github.com/uoguelph-mlrg/Cutout/blob/master/util/cutout.py)
+         normalize])
+else:
+    train_transform = transforms.Compose([])
+    if args.data_augmentation:
+        train_transform.transforms.append(transforms.RandomCrop(32, padding=4))
+        train_transform.transforms.append(transforms.RandomHorizontalFlip())
+    train_transform.transforms.append(transforms.ToTensor())
+    train_transform.transforms.append(normalize)
+    if args.cutout:
+        train_transform.transforms.append(Cutout(n_holes=args.n_holes, length=args.length))
 
 
 test_transform = transforms.Compose([
@@ -107,23 +147,24 @@ elif args.dataset == 'cifar100':
                                      download=True)
 elif args.dataset == 'svhn':
     num_classes = 10
-    train_dataset = datasets.SVHN(root='data/',
+    train_dataset = datasets.SVHN(root=args.data_path,
                                   split='train',
                                   transform=train_transform,
                                   download=True)
 
-    extra_dataset = datasets.SVHN(root='data/',
-                                  split='extra',
-                                  transform=train_transform,
-                                  download=True)
+    if args.use_extra:
+        extra_dataset = datasets.SVHN(root=args.data_path,
+                                      split='extra',
+                                      transform=train_transform,
+                                      download=True)
 
-    # Combine both training splits (https://arxiv.org/pdf/1605.07146.pdf)
-    data = np.concatenate([train_dataset.data, extra_dataset.data], axis=0)
-    labels = np.concatenate([train_dataset.labels, extra_dataset.labels], axis=0)
-    train_dataset.data = data
-    train_dataset.labels = labels
+        # Combine both training splits (https://arxiv.org/pdf/1605.07146.pdf)
+        data = np.concatenate([train_dataset.data, extra_dataset.data], axis=0)
+        labels = np.concatenate([train_dataset.labels, extra_dataset.labels], axis=0)
+        train_dataset.data = data
+        train_dataset.labels = labels
 
-    test_dataset = datasets.SVHN(root='data/',
+    test_dataset = datasets.SVHN(root=args.data_path,
                                  split='test',
                                  transform=test_transform,
                                  download=True)
@@ -133,7 +174,8 @@ train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
                                            batch_size=args.batch_size,
                                            shuffle=True,
                                            pin_memory=True,
-                                           num_workers=2)
+                                           num_workers=2,
+                                           worker_init_fn=random.seed)
 
 test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
                                           batch_size=args.batch_size,
@@ -161,8 +203,31 @@ if args.dataset == 'svhn':
 else:
     scheduler = MultiStepLR(cnn_optimizer, milestones=[60, 120, 160], gamma=0.2)
 
-filename = 'logs/' + test_id + '.csv'
+filename = os.path.join(args.checkpoint, 'log.csv')
 csv_logger = CSVLogger(args=args, fieldnames=['epoch', 'train_acc', 'test_acc'], filename=filename)
+
+
+
+def get_mma_loss(weight):
+    '''
+    :param weight: parameter of model, out_features *　in_features
+    :return: angular loss
+    '''
+    # for convolution layers, flatten
+    if weight.dim() > 2:
+        weight = weight.view(weight.size(0), -1)
+
+    # Dot product of normalized prototypes is cosine similarity.
+    weight_ = F.normalize(weight, p=2, dim=1)
+
+    product = torch.matmul(weight_, weight_.t())
+
+    # Remove diagnonal from loss
+    product_ = product - 2. * torch.diag(torch.diag(product))
+    # Maxmize the minimum theta.
+    loss = -torch.acos(product_.max(dim=1)[0].clamp(-0.99999, 0.99999)).mean()
+
+    return loss
 
 
 def test(loader):
@@ -202,6 +267,18 @@ for epoch in range(args.epochs):
         pred = cnn(images)
 
         xentropy_loss = criterion(pred, labels)
+
+
+        # mma loss
+        if args.mma:
+            for name, m in cnn.named_modules():
+                # if isinstance(m, custom.Con2d_Class):
+                if isinstance(m, (nn.Conv2d, nn.Linear)):
+                    mmaloss = get_mma_loss(m.weight)
+                    if args.mma_weight != 0:
+                        xentropy_loss = xentropy_loss + args.mma_weight * mmaloss
+
+
         xentropy_loss.backward()
         cnn_optimizer.step()
 
@@ -226,5 +303,5 @@ for epoch in range(args.epochs):
     row = {'epoch': str(epoch), 'train_acc': str(accuracy), 'test_acc': str(test_acc)}
     csv_logger.writerow(row)
 
-torch.save(cnn.state_dict(), 'checkpoints/' + test_id + '.pt')
+torch.save(cnn.state_dict(), os.path.join(args.checkpoint, 'model_final.pt'))
 csv_logger.close()
